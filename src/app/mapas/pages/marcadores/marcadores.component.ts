9516547fe4013687917f74f6cd6ff062f7f49fe0
Daniel Meza <danielmeza.4c@gmail.com>
2021-09-21 15:31:44 -0700

    Agregar e ir a ubicación de marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
+//* MapBox
+import * as mapboxgl from 'mapbox-gl';
+
+
+interface MarcadorColor {
+  color: string;
+  marker: mapboxgl.Marker;
+}
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+    .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .list-group {
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        z-index: 99;
+      }
+
+      li {
+        cursor: pointer;
+      }
+
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+
+  mapa!: mapboxgl.Map;
+
+  zoomLevel: number = 13;
+  center: [number, number] = [-109.93178353150681, 27.48371843025329];
+
+  marcadores: MarcadorColor[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+
+    const marker = new mapboxgl.Marker();
   }
+
+  agregarMarcador() {
+
+    const color = "#xxxxxx".replace(/x/g, y => (Math.random() * 16 | 0).toString(16))
+    const nuevoMarcador = new mapboxgl.Marker({ draggable: true, color: color })
+      .setLngLat(this.center)
+      .addTo(this.mapa);
+
+    this.marcadores.push({ color, marker: nuevoMarcador });
+  }
+  irMarcador(marcador: mapboxgl.Marker) {
+    this.mapa.flyTo({
+      center: marcador.getLngLat()
+    });
+  }
+
 
 }
