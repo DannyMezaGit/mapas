12afad78bbf2f616b6bf5193e292a7ada5b0d372
Daniel Meza <danielmeza.4c@gmail.com>
2021-09-21 10:28:04 -0700

    fullmap en el zoom-range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,21 @@
+//* Angular
 import { Component, OnInit } from '@angular/core';
+
+//* MapBox
+import * as mapboxgl from 'mapbox-gl';
+
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+      #mapa{
+        width: 100%;
+        height: 100%;
+      }
+
+  `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +23,14 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -109.93178353150681, 27.48371843025329 ],
+      zoom: 13
+    });
+
   }
 
 }
