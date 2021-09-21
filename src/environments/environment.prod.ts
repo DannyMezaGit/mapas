a85aef6d089c50a17218f3d9c0bcba6739666685
Daniel Meza <danielmeza.4c@gmail.com>
2021-09-20 20:54:19 -0700

    - bootstrap - lazy - componentes de la app - menu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiZGFuaWVsbWV6YWdpbmkiLCJhIjoiY2t0dGprajJ4MThuMDJ1bXhqd2c3dW1seiJ9.VqPF64HXLy6OGpHFCUbU1w'
 };
